9284882a7dad88bea65ea7dee309e21881b69427
Daniel Smith <jellymann@gmail.com>
2013-08-24 12:46:36 +0200

    Added: dependents list to entities.

--- src/main/java/za/co/sourlemon/zambies/ems/Engine.java
+++ src/main/java/za/co/sourlemon/zambies/ems/Engine.java
@@ -10,27 +10,28 @@ import java.util.Map;
  * The core Engine of the Entity Component System. This class store and keeps
  * track of all entities within the system, as well as providing the main loop
  * for executing each sub-system.
- * 
+ *
  * @author daniel
  */
 public class Engine implements EntityListener
 {
+
     private Collection<ISystem> systems = new ArrayList<>();
     private Collection<Entity> entities = new ArrayList<>();
     private Collection<Entity> toAdd = new ArrayList<>();
     private Collection<Entity> toRemove = new ArrayList<>();
     private Map<Class, Collection<Object>> nodeLists = new HashMap<>();
     private Map<Class, Family> families = new HashMap<>();
-    
     boolean updating = false;
-    
+
     /**
-     * Adds an entity to the list of known entities. Automagically creates
-     * and relevant node(s) which is/are added to the appropriate node list.
-     * This method may be called at any time as entities will only be physically
+     * Adds an entity to the list of known entities. Automagically creates and
+     * relevant node(s) which is/are added to the appropriate node list. This
+     * method may be called at any time as entities will only be physically
      * added to the list once any update loop is completed. This allows systems
-     * to add entities during their execution without worrying about
-     * concurrent modification exceptions.
+     * to add entities during their execution without worrying about concurrent
+     * modification exceptions.
+     *
      * @param entity the entity to add to the system
      */
     public void addEntity(Entity entity)
@@ -43,13 +44,16 @@ public class Engine implements EntityListener
         entities.add(entity);
         entity.addEntityListener(this);
         for (Family family : families.values())
+        {
             family.newEntity(entity);
+        }
     }
-    
+
     /**
      * Removes an entity from the list of known entities. Automagically removes
      * any nodes associated with this entity from the appropriate node list(s).
      * Similarly to addEntity, this method may be called safely at any time.
+     *
      * @param entity the entity to remove from the system
      */
     public void removeEntity(Entity entity)
@@ -60,32 +64,44 @@ public class Engine implements EntityListener
             return;
         }
         entity.removeEntityListener(this);
-        
+
         for (Family family : families.values())
+        {
             family.removeEntity(entity);
-        
+        }
+
         entities.remove(entity);
+
+        for (Entity dep : entity.getDependents())
+        {
+            removeEntity(dep);
+        }
     }
 
     @Override
     public void componentAdded(Entity entity, Class componentClass)
     {
         for (Family family : families.values())
+        {
             family.componentAddedToEntity(entity, componentClass);
+        }
     }
 
     @Override
     public void componentRemoved(Entity entity, Class componentClass)
     {
         for (Family family : families.values())
+        {
             family.componentRemovedFromEntity(entity, componentClass);
+        }
     }
-    
+
     /**
      * Gets the list of all instances of the specified Node class
+     *
      * @param <N> the Node Class type
      * @param nodeClass the Node Class
-     * @return the List of all instances of the specified Node class 
+     * @return the List of all instances of the specified Node class
      */
     public <N extends Node> List<N> getNodeList(Class<N> nodeClass)
     {
@@ -101,26 +117,32 @@ public class Engine implements EntityListener
         }
         return family.getNodeList();
     }
-    
+
     /**
-     * Gets the first instance of the specified node class. This is a convenience
-     * method for getting instances of nodes of which only one is expected to
-     * exist. (Sort of like the interface to a kind of "singleton" node.
+     * Gets the first instance of the specified node class. This is a
+     * convenience method for getting instances of nodes of which only one is
+     * expected to exist. (Sort of like the interface to a kind of "singleton"
+     * node.
+     *
      * @param <N> the Node Class type
      * @param nodeClass the node class
-     * @return a single instance of the specified node class 
+     * @return a single instance of the specified node class
      */
     public <N extends Node> N getNode(Class<N> nodeClass)
     {
         List<N> nodes = getNodeList(nodeClass);
-        if (nodes.isEmpty()) return null;
+        if (nodes.isEmpty())
+        {
+            return null;
+        }
         return nodes.get(0);
     }
-    
+
     /**
      * Registers a system to the engine, causing it to be run in the engine's
      * main loop. If an engine's start method fails, the system is not
      * registered to the engine.
+     *
      * @param system the system to add
      */
     public void addSystem(ISystem system)
@@ -130,12 +152,13 @@ public class Engine implements EntityListener
             systems.add(system);
         }
     }
-    
+
     /**
      * The main update loop of the engine. This should be called within a
      * constant loop of some kind, where timekeeping must be done. Delta is
      * meant as a convenience parameter, and its meaning is completely up to the
      * developer.
+     *
      * @param delta the amount of time since the last update.
      */
     public void update(double delta)
@@ -146,37 +169,47 @@ public class Engine implements EntityListener
             s.update(delta);
         }
         updating = false;
-        
+
         for (Entity e : toAdd)
+        {
             addEntity(e);
+        }
         toAdd.clear();
         for (Entity e : toRemove)
+        {
             removeEntity(e);
+        }
         toRemove.clear();
     }
-    
+
     /**
      * De-registers a system from the engine. This will call the system's end()
      * method.
+     *
      * @param system the system to de-register from the engine
      */
     public void removeSystem(ISystem system)
     {
         if (systems.remove(system))
+        {
             system.end();
+        }
     }
-    
+
     /**
      * Shuts down this engine, ending its loop and then calling then end()
      * method of all of the systems.
      */
     public void shutDown()
     {
-        while (updating) { /* spin */ }
-        
+        while (updating)
+        { /* spin */ }
+
         for (ISystem s : systems)
+        {
             s.end();
-        
+        }
+
         systems.clear();
     }
 }
--- src/main/java/za/co/sourlemon/zambies/ems/Entity.java
+++ src/main/java/za/co/sourlemon/zambies/ems/Entity.java
@@ -11,41 +11,62 @@ import java.util.Map;
  */
 public class Entity
 {
+
     Collection<EntityListener> listeners = new ArrayList<>();
     Map<Class, Object> components = new HashMap<>();
-    
+    Collection<Entity> dependents = new ArrayList<>();
+
     public void addEntityListener(EntityListener l)
     {
         listeners.add(l);
     }
-    
+
     public void removeEntityListener(EntityListener l)
     {
         listeners.remove(l);
     }
-    
+
     public void add(Object component)
     {
         Class componentClass = component.getClass();
         components.put(componentClass, component);
-        
+
         for (EntityListener l : listeners)
+        {
             l.componentAdded(this, componentClass);
+        }
     }
-    
+
     public void remove(Class componentClass)
     {
         components.remove(componentClass);
-        
+
         for (EntityListener l : listeners)
+        {
             l.componentRemoved(this, componentClass);
+        }
     }
-    
+
+    public void addDependent(Entity dep)
+    {
+        dependents.add(dep);
+    }
+
+    public void removeDependent(Entity dep)
+    {
+        dependents.remove(dep);
+    }
+
+    public Collection<Entity> getDependents()
+    {
+        return dependents;
+    }
+
     public <C> C get(Class<C> componentClass)
     {
-        return (C)components.get(componentClass);
+        return (C) components.get(componentClass);
     }
-    
+
     public boolean has(Class componentClass)
     {
         return components.containsKey(componentClass);
